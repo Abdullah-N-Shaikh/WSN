e4b15309dda1731da597cabaa96bd6ed82974387
Abdullah-N-Shaikh <Aboode0565759011@gmail.com>
2022-05-05 21:03:08 +0300

    Editing port number

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', (req,res) => {
     res.send("Welcome to my app")
 })
 
-app.listen(3000 , () => {
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
     console.log("hIIIIIIIIIIIIII");
 })
