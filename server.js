be81d06d63922bd2bed7602c4451fbd5a6517a3c
Abdullah-N-Shaikh <Aboode0565759011@gmail.com>
2022-05-06 20:48:13 +0300

    new code passport 3

--- server.js
+++ server.js
@@ -84,4 +84,8 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000)
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
+    console.log("hIIIIIIIIIIIIII");
+})
